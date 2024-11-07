198544d8b73dd7b60faf8b268c08856e857f5d57
ryuchun00 <leokun0210@gmail.com>
2024-11-07 21:31:06 +0900

    スクリプトアップデート

--- src/Frequency/Delayed_Insertion_Sketch/demo/repeat.sh
+++ src/Frequency/Delayed_Insertion_Sketch/demo/repeat.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-input_num=50000
+input_num=500000
 
 # 0.1から1.0まで0.1刻みでループ
 for i in $(seq 0.1 0.1 1.0); do
-  # ./main ../../../../data/sx-stackoverflow_short.txt "$i" # 引数としてiを渡す
-  ./main ../../../../data/artificial2.txt "$i" $input_num # 引数としてiを渡す
+  echo "===================="
+  ./main ../../../../data/sx-stackoverflow.txt "$i" $input_num 5000 # 引数としてiを渡す
+  # ./main ../../../../data/artificial2.txt "$i" $input_num # 引数としてiを渡す
   # ./main ../../../../data/artificial3.txt "$i" # 引数としてiを渡す
 done
--- src/Frequency/Sliding_Sketch_Origin/demo/repeat.sh
+++ src/Frequency/Sliding_Sketch_Origin/demo/repeat.sh
@@ -2,7 +2,8 @@
 
 # 0.1から1.0まで0.1刻みでループ
 for i in $(seq 0.1 0.1 1.0); do
-  # ./main ../../../../data/sx-stackoverflow_short.txt "$i" # 引数としてiを渡す
-  ./main ../../../../data/artificial2.txt "$i" # 引数としてiを渡す
+  echo "===================="
+  ./main ../../../../data/sx-stackoverflow.txt "$i" 500000 # 引数としてiを渡す
+  # ./main ../../../../data/artificial2.txt "$i" $1 # 引数としてiを渡す
   # ./main ../../../../data/artificial3.txt "$i" # 引数としてiを渡す
 done
